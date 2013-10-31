bfe91e4673218a23382e4180cc880e14b56b524c
bjornlogi <bls4@hi.is>
2013-10-31 13:18:48 +0000

    Hreinsun á entity manager

---
+++ entityManager.js
@@ -0,0 +1,79 @@
+"use strict";
+
+
+var entityManager = {
+
+
+_rocks   : [],
+_bullets : [],
+_ships   : [],
+
+// "PRIVATE" METHODS
+
+_generateRocks : function() {
+    var i,
+        NUM_ROCKS = 4;
+
+    for (i = 0; i < NUM_ROCKS; ++i) {
+        this.generateRock();
+    }
+},
+
+_forEachOf: function(aCategory, fn) {
+    for (var i = 0; i < aCategory.length; ++i) {
+        fn.call(aCategory[i]);
+    }
+},
+
+update: function(du) {
+
+    for (var c = 0; c < this._categories.length; ++c) {
+
+        var aCategory = this._categories[c];
+        var i = 0;
+
+        while (i < aCategory.length) {
+
+            var status = aCategory[i].update(du);
+
+            if (status === this.KILL_ME_NOW) {
+                // remove the dead guy, and shuffle the others down to
+                // prevent a confusing gap from appearing in the array
+                aCategory.splice(i,1);
+            }
+            else {
+                ++i;
+            }
+        }
+    }
+    
+    if (this._rocks.length === 0) this._generateRocks();
+
+},
+
+render: function(ctx) {
+
+    var debugX = 10, debugY = 100;
+
+    for (var c = 0; c < this._categories.length; ++c) {
+
+        var aCategory = this._categories[c];
+
+        if (!this._bShowRocks && 
+            aCategory == this._rocks)
+            continue;
+
+        for (var i = 0; i < aCategory.length; ++i) {
+
+            aCategory[i].render(ctx);
+            //debug.text(".", debugX + i * 10, debugY);
+
+        }
+        debugY += 10;
+    }
+}
+
+}
+
+// Some deferred setup which needs the object to have been created first
+entityManager.deferredSetup();
