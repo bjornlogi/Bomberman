8f5cfc82989fef08ab29faaf0dd81e19bbd79c82
bjornlogi <bls4@hi.is>
2013-10-31 12:49:53 +0000

    eyddi commentum
    er adallega ad profa hvort tetta virki

--- entityManager.js
+++ entityManager.js
@@ -1,26 +1,5 @@
-/*
-
-entityManager.js
-
-A module which handles arbitrary entity-management for "Asteroids"
-
-
-We create this module as a single global object, and initialise it
-with suitable 'data' and 'methods'.
-
-"Private" properties are denoted by an underscore prefix convention.
-
-*/
-
 
 "use strict";
-
-
-// Tell jslint not to complain about my use of underscore prefixes (nomen),
-// my flattening of some indentation (white), or my use of incr/decr ops 
-// (plusplus).
-//
-/*jslint nomen: true, white: true, plusplus: true*/
 
 
 var entityManager = {
