45b399d69607abde0bd1a0199c049256176fd7a4
bjornlogi <bls4@hi.is>
2013-11-19 23:02:12 +0000

    indent

--- util.js
+++ util.js
@@ -302,15 +302,15 @@ findNearestSpotForBomb : function(cx,cy){
 // ==========
 
 clearCanvas: function (ctx) {
-        var base_image = new Image();
-        base_image.src = 'https://notendur.hi.is/~pap5/bomberman/sprite/a_grass_background_1.jpg';
-        base_image.onload = function(){
+    var base_image = new Image();
+    base_image.src = 'https://notendur.hi.is/~pap5/bomberman/sprite/a_grass_background_1.jpg';
+    base_image.onload = function(){
         ctx.drawImage(base_image, 0, 0);
     }
     var base_image = new Image();
     base_image.src = 'https://notendur.hi.is/~pap5/bomberman/sprite/starscape.png';
     base_image.onload = function(){
-    ctx.drawImage(base_image, 0, 0);
+        ctx.drawImage(base_image, 0, 0);
   }
 },
 
