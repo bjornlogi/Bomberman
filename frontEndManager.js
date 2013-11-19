d163ea6267d172e386d0f8b0293008f92c0bf94a
Páll Arnar <palarn@gmail.com>
2013-11-19 22:20:20 +0000

    Smá mynd fyrir opponent

--- frontEndManager.js
+++ frontEndManager.js
@@ -49,14 +49,19 @@ var frontEndManager = {
   },
  
  renderOpponentScreen : function (ctx){
-    util.fillBox(ctx, 0,0,600,600,"black");
+    //util.fillBox(ctx, 0,0,600,600,"black");
+    var opponent_image = new Image();
+      opponent_image.src = 'https://notendur.hi.is/~pap5/bomberman/pic/opponent.png';
+      opponent_image.onload = function(){
+        ctx.drawImage(opponent_image, 0, 0);
+    } 
     var b1 = this.P1_Button;
     var b2 = this.P2_Button;
     var b1_TLeft = util.getTopLeftCorner(b1.cx, b1.cy, b1.halfWidth, b1.halfHeight);
     var b2_TLeft = util.getTopLeftCorner(b2.cx, b2.cy, b2.halfWidth, b2.halfHeight);
     //console.log(b1_TLeft.x, b1_TLeft.y, b1.halfWidth*2, b1.halfHeight*2)
-    util.fillBox (ctx, b1_TLeft.x, b1_TLeft.y, b1.halfWidth*2, b1.halfHeight*2, "white" );
-    util.fillBox (ctx, b2_TLeft.x, b2_TLeft.y, b2.halfWidth*2, b2.halfHeight*2, "white" );
+    //util.fillBox (ctx, b1_TLeft.x, b1_TLeft.y, b1.halfWidth*2, b1.halfHeight*2, "white" );
+    //util.fillBox (ctx, b2_TLeft.x, b2_TLeft.y, b2.halfWidth*2, b2.halfHeight*2, "white" );
 
  },
 
