6238b9a8e336c03c50d8f81366d0ab57a2ac046b
bjornlogi <bls4@hi.is>
2013-11-20 13:01:01 +0000

    liveboard

--- bomberman.js
+++ bomberman.js
@@ -424,7 +424,7 @@ function createThemeSprites(theme){
 function createScoreBoard(){
     console.log(g_sheets.scoreboard)
     sprites.scoreboard.push(new Sprite(g_sheets.scoreboard, 0, 0,
-                        200, 59));
+                        184, 100));
     frontEndManager.scoreboardSprite = sprites.scoreboard[0];
 }
 
