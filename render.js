5087f6057123e87de1c76bc779528d5f95bceb4a
bjornlogi <bls4@hi.is>
2013-11-26 16:31:13 +0000

    hreinsun á gömlum debug breytum

--- render.js
+++ render.js
@@ -1,18 +1,5 @@
 // GENERIC RENDERING
 
-var g_doClear = true;
-var g_doBox = false;
-var g_undoBox = false;
-var g_doFlipFlop = false;
-var g_doRender = true;
-
-var g_frameCounter = 1;
-
-var TOGGLE_CLEAR = 'C'.charCodeAt(0);
-var TOGGLE_BOX = 'B'.charCodeAt(0);
-var TOGGLE_UNDO_BOX = 'U'.charCodeAt(0);
-var TOGGLE_FLIPFLOP = 'F'.charCodeAt(0);
-var TOGGLE_RENDER = 'R'.charCodeAt(0);
 
 function render(ctx, sctx) {
     
